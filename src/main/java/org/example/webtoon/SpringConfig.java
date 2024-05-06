4a62bff0f83309e61c3d9f215c38cfe422c75e27
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-05-06 23:42:02 +0900

    Feat: implement inject bean
    수동 빈 주입 기능 구현

---
+++ src/main/java/org/example/webtoon/SpringConfig.java
@@ -0,0 +1,54 @@
+package org.example.webtoon;
+
+import javax.sql.DataSource;
+
+import org.example.webtoon.repository.MemberRepository;
+import org.example.webtoon.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.persistence.EntityManager;
+
+@Configuration
+public class SpringConfig {
+
+	private final DataSource dataSource;
+	private final EntityManager em;
+
+	@Autowired
+	public SpringConfig(EntityManager em, DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.em = em;
+	}
+
+	@Bean
+	public MemberService memberService() {
+		return new MemberService(memberRepository());
+	}
+
+	@Bean
+	public MemberRepository memberRepository() {
+		return new MemberRepository(em);
+	}
+/*
+	@Bean
+	public WebtoonService webtoonService() {
+		return new WebtoonService(webtoonRepository());
+	}
+
+	@Bean
+	public WebtoonRepository webtoonRepository() {
+		return new WebtoonRepository(em);
+	}
+
+	@Bean
+	public ReviewService reviewService() {
+		return new ReviewService(reviewRepository());
+	}
+
+	@Bean
+	public ReviewRepository reviewRepository() {
+		return new JpaReviewRepository(em);
+	}*/
+}
