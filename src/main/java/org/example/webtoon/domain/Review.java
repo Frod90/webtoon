f5bf291eaf7284f4f811c15d0d75785362fccc54
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-05-09 12:29:42 +0900

    Feat: implement review DTO

---
+++ src/main/java/org/example/webtoon/domain/Review.java
@@ -0,0 +1,59 @@
+package org.example.webtoon.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class Review {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long ID;
+
+	private String title;
+	private String content;
+	private String userID;
+	private String webtoonID;
+
+	public void setID(long ID) {
+		this.ID = ID;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public long getID() {
+		return ID;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setWebtoonID(String webtoonID) {
+		this.webtoonID = webtoonID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWebtoonID() {
+		return webtoonID;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
