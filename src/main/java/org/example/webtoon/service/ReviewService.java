0063e461f9880893055856a4ad6c5b80359a82e7
Frod90 <142146516+Frod90@users.noreply.github.com>
2024-05-09 12:30:07 +0900

    Feat: implement review service

---
+++ src/main/java/org/example/webtoon/service/ReviewService.java
@@ -0,0 +1,32 @@
+package org.example.webtoon.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.example.webtoon.domain.Review;
+import org.example.webtoon.repository.ReviewRepository;
+
+import jakarta.transaction.Transactional;
+
+@Transactional
+public class ReviewService {
+
+	 private final ReviewRepository reviewRepository;
+
+	public ReviewService(ReviewRepository reviewRepository) {
+		this.reviewRepository = reviewRepository;
+	}
+
+	public void save(Review review) {
+
+		reviewRepository.save(review);
+	}
+
+	public List<Review> findByUserId(String id) {
+		return reviewRepository.findByUserId(id);
+	}
+
+	public List<Review> findByWebtoonID(String webtoonId) {
+		return reviewRepository.findByWebtoonID(webtoonId);
+	}
+}
